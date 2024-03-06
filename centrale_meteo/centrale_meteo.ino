cc7336578cd435a7511d1424fa253e0ebf42d7d3
ritonun <73615644+ritonun@users.noreply.github.com>
2024-03-06 15:23:39 +0100

    implementation ariquality + puieviometre

--- centrale_meteo/centrale_meteo.ino
+++ centrale_meteo/centrale_meteo.ino
@@ -5,6 +5,12 @@
 #include "AirQuality.h"
 #include "Si115X.h"
 
+// PINOUT
+#define PIN_PLUIE       A0
+#define PIN_GIROUETTE   A1
+#define PIN_ANEMOMETRE  A2
+#define PIN_AIRQUALI    A3       
+
 // Const du programme
 #define DHTPIN 2
 #define DHTTYPE DHT11   // Modele du capteur avec lequel on travaille
@@ -14,6 +20,8 @@ DHT dht(DHTPIN, DHTTYPE); // Init la classe DHT
 rgb_lcd lcd;  // Init la class ecran
 AirQuality airqualitysensor;  // Init la class airquality
 Si115X si1151; // init la class st114X pour le capteur  de luminosite
+
+float niveau_pluie = 0.0;
 
 void capteur_luminosite() {
     Serial.print("IR: ");
@@ -38,6 +46,20 @@ void capteur_airquality() {
   }
 }
 
+void setup_capteur_airquality() {
+    // Init capteur sunlight
+    if (!si1151.Begin(false)) {
+        Serial.println("Si1151 is not ready!");
+        while (1) {
+            delay(1000);
+            Serial.print(".");
+        };
+    }
+    else {
+        Serial.println("Si1151 is ready!");
+    }
+}
+
 void capteur_dht() {
   // Fonction pour récuperer la donnée du capteur DHT de température et humidité
  
@@ -55,33 +77,82 @@ void capteur_dht() {
   } 
 }
 
+void test_pluviometre(){
+  int BP = digitalRead(A0); // Lecture du capteur
+  Serial.print(BP);
+  if (BP == LOW) {
+    digitalWrite(13, HIGH); // Allume la Led
+    Serial.println("BP HIGH");
+  }
+  else {
+    digitalWrite(13, LOW); // Eteind la Led
+    Serial.println("BP LOW");
+  }
+}
+
+void pluviometre() {
+  const float capacite_bp = 0.2794;  // mm
+  static float last_position = 0.0;
+  static int nombre_de_vidage = 0;
+  int BP = digitalRead(A0); // Lecture du capteur
+  if (BP == LOW) {
+    if (BP != last_position) {
+      if (nombre_de_vidage % 2 == 0) {
+        niveau_pluie += capacite_bp;
+        nombre_de_vidage++;
+      }
+      else {
+        nombre_de_vidage++;
+      }
+    }
+  }
+  last_position = BP;
+}
+
+void anemometre() {
+  int BP = digitalRead(A2); // Lecture du capteur
+  Serial.print("Anemometre: ");
+  Serial.println(BP);
+}
+
+void girouette() {
+  int sensorValue = analogRead(A1);
+  Serial.print("Girouette: ");
+  Serial.println(sensorValue);
+}
+
 void test_analog() {
   int valeur_pot = analogRead(A0);
   Serial.println(valeur_pot);
+}
+
+void print_ecrant(const char string[]) {
+  lcd.print(string);
 }
 
 void setup() {
   Serial.begin(115200); // Initialise la communication via le cable entre l'arduino et le pc
   Wire.begin(); // Initialise I2C
 
-  dht.begin();  // Start le capteur dht
+  //dht.begin();  // Start le capteur dht
 
-  //airqualitysensor.init(A0);  // Init le capteur de qualite de l air
+  //setup_capteur_airquality();
 
-  // Init capteur sunlight
-    if (!si1151.Begin()) {
-        Serial.println("Si1151 is not ready!");
-        while (1) {
-            delay(1000);
-            Serial.print(".");
-        };
-    }
-    else {
-        Serial.println("Si1151 is ready!");
-    }
+  //airqualitysensor.init(A3);  // Init le capteur de qualite de l air
+
+  // pluviometre
+  pinMode(13, OUTPUT);
+  pinMode(A0, INPUT);
+
+  // anenometre
+  pinMode(A2, INPUT);
+
+  //girouette
+  pinMode(A1, INPUT);
+
+  lcd.begin(16,2);  // Set up l'ecran de taille 16 colonne 2 lignes (16x2 characteres)
   
-  lcd.begin(16,2);  // Set up l'ecran de taille 16 colonne 2 lignes
-  lcd.print("Setup OK!");
+  print_ecrant("Setup OK");
   Serial.println("Setup OK"); // Pas d'erreur dans le setup
 }
 
@@ -89,8 +160,19 @@ void loop() {
   //capteur_dht();
   //capteur_airquality();
   //test_analog();
-  capteur_luminosite();
-  delay(2000);
+  //capteur_luminosite();
+  //pluviometre();
+  //girouette();
+  //anemometre();
+  /*
+  Serial.print("Niv pluie");
+  Serial.println(niveau_pluie);
+  */
+  print_ecrant("loop");
+  delay(1000);
+  lcd.clear();
+  print_ecrant("clear");
+  delay(1000);
 }
 
 ISR(TIMER2_OVF_vect)
